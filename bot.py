833734ba60e79482a30f74372209741ce2a5cb87
Evyatar Cohen <coevyatar@gmail.com>
2020-07-28 20:20:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,39 +7,43 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 GUILD = os.getenv("DISCORD_GUILD")
+TARGET_USER = os.getenv("TARGET_USER")
 
 client = discord.Client()
 
-CURSES_EN = [
-  "shutup",
-  "cos emek",
-]
+CURSES_EN = []
+ANSWERS_EN = []
 
-CURSES_HE = [
-  "שתוק",
-  "סתום",
-  "אמא שלך",
-]
-
-ANSWERS_EN = [
-  "Ohhhhhhhh, what a dumb thing to say",
-  "Thats it? thats your best?",
-  "Lame.",
-]
-
-ANSWERS_HE = [
-  "באמת? זה הכי טוב שלך?",
-  "תשמע, אתה גרוע",
-  "וואי וואי, איזה אפס אתה תאמין לי",
-]
+CURSES_HE = []
+ANSWERS_HE = []
 
 @client.event
 async def on_ready():
   guild = discord.utils.get(client.guilds, name=GUILD)
+  
+  try:
+    with open("configs/en/curses.txt", "r") as en_curse_stream:
+      for line in en_curse_stream:
+        CURSES_EN.append(line.strip())
     
+    with open("configs/en/answers.txt", "r") as en_answer_stream:
+      for line in en_answer_stream:
+        ANSWERS_EN.append(line.strip())
+    
+    with open("configs/he/curses.txt", "r") as he_curse_stream:
+      for line in he_curse_stream:
+        CURSES_HE.append(line.strip())
+    
+    with open("configs/he/answers.txt", "r") as he_answer_stream:
+      for line in he_answer_stream:
+        ANSWERS_HE.append(line.strip())
+
+  except Exception as ex:
+    print("Error")
+
   print(
     f'{client.user} is connected to the following guild:\n'
-    f'{guild.name}(id: {guild.id})'
+    f'{guild.name}(id: {guild.id})\n'
   )
 
 @client.event
@@ -48,14 +52,14 @@ async def on_message(message):
     return
 
   username = client.get_user(message.author.id)
-  if "orel" in username.name.lower():
-    if message.content in CURSES_EN:
+  if TARGET_USER in username.name.lower():
+    if any(curse in message.content for curse in CURSES_EN):
       response = random.choice(ANSWERS_EN)
       await message.channel.send(f'{username.mention} - {response}')
-    if message.content in CURSES_HE:
+    elif any(curse in message.content for curse in CURSES_HE):
       response = random.choice(ANSWERS_HE)
       await message.channel.send(f'{username.mention} - {response}')
   else:
-    print(f'{username} is not orel')
+    print(f'{username} is not {TARGET_USER}')
 
 client.run(TOKEN)
